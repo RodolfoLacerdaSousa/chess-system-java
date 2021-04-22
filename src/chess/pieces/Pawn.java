be5f01459821da7a3ba0d92703405ac0cd2be3a9
Rodolfo Lacerda <rodolfolacerdajp@gmail.com>
2021-04-22 14:59:58 -0300

    Queen (and Pawn bugfix)

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -50,7 +50,7 @@ public class Pawn extends ChessPiece{
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 			p.setValues(position.getRow()+2, position.getColumn());
-			Position p2 = new Position(position.getRow()-1, position.getColumn()); //movimento incial q pode andar 2 casa, ai testa se tem algo na primeira ou na segunda.
+			Position p2 = new Position(position.getRow()+1, position.getColumn()); //movimento incial q pode andar 2 casa, ai testa se tem algo na primeira ou na segunda.
 			if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount()==0) {
 				mat[p.getRow()][p.getColumn()] = true;						
 			}
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -68,9 +68,4 @@ public class Rook extends ChessPiece {
 		
 		return mat;
 	}
-	
-	
-	
-	
-	
 }
