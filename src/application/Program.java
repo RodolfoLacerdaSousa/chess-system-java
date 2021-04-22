b418797de13597c1cc9ed35905ce19b3b7a5f500
Rodolfo Lacerda <rodolfolacerdajp@gmail.com>
2021-04-22 17:17:27 -0300

    Fix - better treatment to promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -45,7 +45,11 @@ public class Program {
 				
 				if (chessMatch.getPromoted() != null) { //siginifica q uma peça foi promovida
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
-					String type = sc.nextLine();
+					String type = sc.nextLine().toUpperCase(); // para converter a letra para maiusculo
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Invalid Value. Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 			}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -135,7 +134,7 @@ public class ChessMatch { // regras do jogo
 			throw new IllegalStateException("There is no piece to be promoted");
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) { //Com String usa EQUALS apra saber se eh igual, pq STRING nao eh 1 tipo primitivo(Ex> int num == 2; ou char ch == 'a')
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
